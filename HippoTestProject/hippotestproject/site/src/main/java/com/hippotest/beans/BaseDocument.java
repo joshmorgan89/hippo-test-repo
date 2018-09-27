2e0d9e49ffbb666c18a1615004d4d54acb54e36e
jmorgan <joshua.morgan@cengage.com>
2018-09-27 15:52:50 -0500

    Initial commit

---
+++ HippoTestProject/hippotestproject/site/src/main/java/com/hippotest/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.hippotest.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippotestproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
